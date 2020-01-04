16491e1b5a68b6665717a6b2e86c1a2d23786e57
hamedmirzaei <mirzaeihamed1368@gmail.com>
2020-01-04 16:36:14 +0330

    extend: using ConfigurationProperties annotation to parametrize maintenance time period

--- src/main/java/spring/boot/entity/dto/configs/interceptor/MaintenanceTimeInterceptor.java
+++ src/main/java/spring/boot/entity/dto/configs/interceptor/MaintenanceTimeInterceptor.java
@@ -1,14 +1,32 @@
 package spring.boot.entity.dto.configs.interceptor;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.Calendar;
 
 @Component
+@ConfigurationProperties("maintenance")
+@Validated
+@Getter
+@Setter
 public class MaintenanceTimeInterceptor implements HandlerInterceptor {
+
+    @Max(24)
+    @Min(0)
+    private int timeFrom;
+    @Max(24)
+    @Min(0)
+    private int timeTo;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // skip swaggers
@@ -17,7 +35,7 @@ public class MaintenanceTimeInterceptor implements HandlerInterceptor {
         // check for maintenance time to handle it
         Calendar cal = Calendar.getInstance();
         int hour = cal.get(cal.HOUR_OF_DAY);
-        if (hour >= 23 && hour <= 24) {
+        if (hour >= timeFrom && hour <= timeTo) {
             if (request.getRequestURI().startsWith("/error"))
                 return true;
             else {
